9812d60402f7defc578fe631b679ce421531a67c
Roman Cherniakhovsky <bizrenay@gmail.com>
2020-04-16 17:55:01 +0300

    add items

--- src/view/component/ChestInsides.vue
+++ src/view/component/ChestInsides.vue
@@ -1,7 +1,11 @@
 <template>
     <div class="chest-inside">
         <div class="chest-inside__more-text">Предметы, которые могут быть в этом сундуке:</div>
-
+        <div class="chest-inside__container">
+            <div v-for="item in items" :key="item.id" class="chest-inside__item">
+                <p class="title"></p>
+            </div>
+        </div>
         <div class="chest-inside__footer">
             <div class="chest-inside__buttons">
                 <button class="btn btn-green">Открыть Сундук</button>
@@ -18,6 +22,104 @@
     @Component
     export default class extends Vue
     {
+        private items: any | object
+
+        created(): void
+        {
+            this.items = [
+                {
+                    id: 1,
+                    title: "Vip",
+                    image: "",
+                },
+                {
+                    id: 2,
+                    title: "Premium",
+                    image: "",
+                },
+                {
+                    id: 3,
+                    title: "Creative",
+                    image: "",
+                },
+                {
+                    id: 4,
+                    title: "Admin",
+                    image: "",
+                },
+                {
+                    id: 5,
+                    title: "Lord",
+                    image: "",
+                },
+                {
+                    id: 6,
+                    title: "Enigma",
+                    image: "",
+                },
+                {
+                    id: 7,
+                    title: "Deluxe",
+                    image: "",
+                },
+                {
+                    id: 8,
+                    title: "Caesar",
+                    image: "",
+                },
+                {
+                    id: 9,
+                    title: "Baron",
+                    image: "",
+                },
+                {
+                    id: 10,
+                    title: "Hero",
+                    image: "",
+                },
+                {
+                    id: 11,
+                    title: "Sentinel",
+                    image: "",
+                },
+                {
+                    id: 12,
+                    title: "Knight",
+                    image: "",
+                },
+                {
+                    id: 13,
+                    title: "Emperor",
+                    image: "",
+                },
+                {
+                    id: 14,
+                    title: "Czar",
+                    image: "",
+                },
+                {
+                    id: 15,
+                    title: "King",
+                    image: "",
+                },
+                {
+                    id: 16,
+                    title: "Ruler",
+                    image: "",
+                },
+                {
+                    id: 17,
+                    title: "Vice President",
+                    image: "",
+                },
+                {
+                    id: 18,
+                    title: "President",
+                    image: "",
+                },
+            ]
+        }
+
         mounted(): void
         {
         }
@@ -26,7 +128,7 @@
 
 <style lang="stylus" scoped>
     $moreTextFS = 18px
-    $linePositionY = $moreTextFS + $moreTextFS/2px
+    $linePositionY = $moreTextFS + $moreTextFS/ 2px
 
     .chest-inside
         position: absolute
@@ -35,6 +137,7 @@
         bottom 0
         left 5%
         right 5%
+
         &:before
             width 100%
             position absolute
@@ -44,6 +147,7 @@
             left 0
             background-image radial-gradient(rgba(255, 255, 255, .1), rgba(255, 255, 255, .4), rgba(255, 255, 255, 1))
             box-shadow -20px 5px 11px 2px rgba(0, 0, 0, 0.5)
+
         &__more-text
             font-family 'M PLUS Rounded 1c', sans-serif
             text-align left
@@ -52,13 +156,16 @@
             background linear-gradient(20deg, rgba(255, 255, 255, 1), rgba(255, 255, 255, .35), rgba(255, 255, 255, .05))
             -webkit-background-clip text
             -webkit-text-fill-color transparent
+
         &__footer
             position absolute
             bottom 20px
             width 100%
             left 0
+
         &__buttons
             float right
+
             .btn:first-child
                 margin-right 20px
 </style>
