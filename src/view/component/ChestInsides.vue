136afa8da245224ab4bde645d6424ef9b727a1d6
Roman Cherniakhovsky <bizrenay@gmail.com>
2020-04-23 20:15:27 +0300

    change media request

--- src/view/component/ChestInsides.vue
+++ src/view/component/ChestInsides.vue
@@ -183,7 +183,10 @@
                 &:before
                     top $linePositionY + 26px
                     left -10px
-
+    @media screen and (max-height: 320px)
+        .chest-inside
+            &__container
+                display none
     @media screen and (min-width: 140px) and (max-width: 639px)
         .chest-inside
             &__item
@@ -203,10 +206,6 @@
                 bottom 10px
                 width 100%
                 right 0
-    @media screen and (max-height: 640px)
-        .chest-inside
-            &__container
-                display none
     @media screen and (max-width: 640px)
         .chest-inside
             &__footer
